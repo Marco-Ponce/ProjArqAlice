d6153b8c459503145d0a5019f1f94aa691136213
Marco Macedo <marcomacedo2000pe@gmail.com>
2025-03-04 23:24:19 -0300

    Primeiro commit

---
+++ assets/js/app.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let about = document.getElementById("about");
+    let aboutText = document.getElementById("aboutText");
+
+    // Aplicando o background gradient no #about
+    about.style.background = "linear-gradient(180deg, rgba(0, 0, 0, 0) 16%, rgba(0, 0, 0, 0.85) 62%)";
+   
+
+    // Configuração inicial para ocultar suavemente o aboutText
+    aboutText.style.opacity = "0";
+    aboutText.style.visibility = "hidden";
+    aboutText.style.transition = "opacity 0.5s ease, visibility 0.5s ease";
+
+    about.onmouseover = function showAbout() {
+        aboutText.style.opacity = "1";
+        aboutText.style.visibility = "visible";
+    };
+
+    about.onmouseout = function hideAbout() {
+        aboutText.style.opacity = "0";
+        aboutText.style.visibility = "hidden";
+    };
+});
